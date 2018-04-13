0f81cbd14e45089a03050a772704b59ebecb7d64
Raruto <Theo.95@hotmail.it>
2018-04-13 23:27:14 +0200

    TODO added:"poll wp-packagist"

--- src/GitHub_Updater/Model/Plugin/WordpressPlugin.php
+++ src/GitHub_Updater/Model/Plugin/WordpressPlugin.php
@@ -22,6 +22,10 @@ abstract class WordpressPlugin implements PluginInterface {
 	protected $filepath;		// eg. www.example.com/wp-content/plugins/akismet/akismet.php
 	protected $plugin_data;
 
+	//TODO: Not all plugins are on WPackagist,
+	// we should look at RepositoryInterface::findPackage
+	// in the Composer APIs to ascertain if a package
+	// is present or not (see also: https://github.com/Raruto/composerpress)
 	const FALLBACK_VENDOR = "wpackagist-plugin";
 
 	public function __construct( $plugin_path, $plugin_data ){
